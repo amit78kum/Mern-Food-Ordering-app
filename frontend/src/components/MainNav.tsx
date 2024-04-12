afc8e4f6bfd0d1ef3caf995f4ba0295588bd0637
amit78kum <amkumar7488@gmail.com>
2024-04-12 22:43:47 +0530

    Authenticated user links

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,21 +1,21 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
-//import UsernameMenu from "./UsernameMenu";
+import UsernameMenu from "./UsernameMenu";
 import { Link } from "react-router-dom";
 
 const MainNav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect,isAuthenticated } = useAuth0();
 
   return (
     <span className="flex space-x-2 items-center">
-      {/* {isAuthenticated ? (
+       {isAuthenticated ? (
         <>
           <Link to="/order-status" className="font-bold hover:text-orange-500">
             Order Status
           </Link>
           <UsernameMenu />
         </>
-      ) : ( */}
+      ) : ( 
         <Button
           variant="ghost"
           className="font-bold hover:text-orange-500 hover:bg-white"
@@ -23,7 +23,7 @@ const MainNav = () => {
         >
           Log In
         </Button>
-      {/* )} */}
+       )} 
     </span>
   );
 };
